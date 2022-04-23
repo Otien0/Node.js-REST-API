e87dbd8e6270dcafa63a7d4f06bd758645a5fbd4
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 23:55:51 +0300

    use toObject global middleware into service

--- node-api/service/productsService.js
+++ node-api/service/productsService.js
@@ -1,13 +1,25 @@
-const Product        = require('../models/productModel')
+const Product           = require('../models/productModel'),
+      {formatMongoData} = require('../helper/dbHelper');
 
 module.exports.createProduct = async(serviceData) => {
     try {
         let product = new Product({... serviceData})
 
         let result =  await product.save()
-        return result.toObject();
+        return formatMongoData(result);
     } catch (error) {
         console.log('Something went wrong: Service: createProduct', error)
+        throw new Error(error)
+    }
+   
+}
+
+module.exports.getProducts = async(serviceData) => {
+    try {
+        let products = await Product.find({})
+        return formatMongoData(products)
+    } catch (error) {
+        console.log('Something went wrong: Service: getProducts', error)
         throw new Error(error)
     }
    
