e89f18e915cda7099b00bbe6e1ca42539870062b
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 20:35:16 +0300

    update productModel

--- node-api/models/productModel.js
+++ node-api/models/productModel.js
@@ -14,7 +14,16 @@ const productSchema = new Schema({
     },
     brand : String
 }, {
-    timestamps: true
+    timestamps: true,
+    toObject: {
+        transform: function(doc, ret, options) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+
+        }
+    }
 })
 
 module.exports = mongoose.model('Product', productSchema);
