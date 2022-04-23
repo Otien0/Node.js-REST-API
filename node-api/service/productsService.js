832f6ebb138199d7e5d4476dde814cc7ddf73c58
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 20:36:11 +0300

    update productService

--- node-api/service/productsService.js
+++ node-api/service/productsService.js
@@ -3,7 +3,9 @@ const Product        = require('../models/productModel')
 module.exports.createProduct = async(serviceData) => {
     try {
         let product = new Product({... serviceData})
-        return await product.save()
+
+        let result =  await product.save()
+        return result.toObject();
     } catch (error) {
         console.log('Something went wrong: Service: createProduct', error)
         throw new Error(error)
