097918b3090ce82ee6a6f808073836cf2fe573fe
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 23:17:19 +0300

    added a products route

--- node-api/routes/products.js
+++ node-api/routes/products.js
@@ -3,11 +3,11 @@ const express               = require("express"),
     //   Product               = require("../models/productModel"),
       products              = require('../controllers/productsController'),
       { validateProduct }   = require('../middleware/validations'),
-      Schema                = require('../apiSchema/JoiSchemas');
+      product               = require('../apiSchema/JoiSchemas');
 
 
 
-router.post('/', validateProduct(Schema.ProductSchema), products.createProduct);
+router.post('/', validateProduct(product.ProductSchema), products.createProduct);
 
 
 module.exports = router;
