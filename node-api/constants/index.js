1657a8b394f9aed71a70ccb356fb3dbe202f7379
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 13:42:37 +0300

    added a constant response message route

---
+++ node-api/constants/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    defaultServerResponse: {
+        statusCode: 400,
+        message: '',
+        body: {}
+    },
+    productMessage: {
+        PRODUCT_CREATED: 'Product Created Successfully'
+    }
+}
