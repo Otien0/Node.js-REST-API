2d40e88dafee3bca74e74261e2bb629c2158aa9a
mourice-oduor <otienomaurice364@gmail.com>
2022-04-24 20:23:05 +0300

    User signup POST Controller

---
+++ node-api/controllers/usersController.js
@@ -0,0 +1,84 @@
+const userService    = require('../service/userService'),
+      constants      = require('../constants');
+
+
+//CREATE ROUTE-- Add new Product to DB
+module.exports.signup = async(req, res) => {
+    let response = {...constants.defaultServerResponse}
+
+    try {
+        const serviceResponse = await userService.signup(req.body);
+        response.statusCode = 200;
+        response.message = constants.userMessage.SIGNUP_SUCCESS;
+        response.body = serviceResponse;
+    } catch (error) {
+        console.log('Something went wrong: Controller: signup', error);
+        response.message = error.message;
+    }
+    return res.status(response.statusCode).send(response);
+}
+
+//GET ROUTE-- GET all Products from the Database
+// module.exports.getProducts = async (req, res,) => {
+//     let response = {...constants.defaultServerResponse }
+
+//     try {
+//         const serviceResponse = await productService.getProducts(req.query);
+//         response.statusCode = 200;
+//         response.message = constants.productMessage.PRODUCT_FETCHED;
+//         response.body = serviceResponse;
+//     } catch (error) {
+//         console.log('Something went wrong: Controller: getProducts', error);
+//         response.message = error.message;
+//     }
+//     return res.status(response.statusCode).send(response);
+// }
+
+//GET ROUTE-- GET a single Product By Id from the Database
+// module.exports.getProductById = async (req, res,) => {
+//     let response = {...constants.defaultServerResponse }
+
+//     try {
+//         const serviceResponse = await productService.getProductById(req.params);
+//         response.statusCode = 200;
+//         response.message = constants.productMessage.PRODUCT_FETCHED;
+//         response.body = serviceResponse;
+//     } catch (error) {
+//         console.log('Something went wrong: Controller: getProductById', error);
+//         response.message = error.message;
+//     }
+//     return res.status(response.statusCode).send(response);
+// }
+
+// module.exports.updateProduct = async (req, res,) => {
+//     let response = {...constants.defaultServerResponse }
+
+//     try {
+//         const serviceResponse = await productService.updateProduct({
+//             id: req.params.id,
+//             updateInfo: req.body
+//         });
+//         response.statusCode = 200;
+//         response.message = constants.productMessage.PRODUCT_UPDATED;
+//         response.body = serviceResponse;
+//     } catch (error) {
+//         console.log('Something went wrong: Controller: getProductById', error);
+//         response.message = error.message;
+//     }
+//     return res.status(response.statusCode).send(response);
+// }
+
+// module.exports.deleteProduct = async (req, res,) => {
+//     let response = {...constants.defaultServerResponse }
+
+//     try {
+//         const serviceResponse = await productService.deleteProduct(req.params);
+//         response.statusCode = 200;
+//         response.message = constants.productMessage.PRODUCT_DELETED;
+//         response.body = serviceResponse;
+//     } catch (error) {
+//         console.log('Something went wrong: Controller: deleteProduct', error);
+//         response.message = error.message;
+//     }
+//     return res.status(response.statusCode).send(response);
+// }
