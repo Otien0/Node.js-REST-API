0692cd77e4dc1f38c52d18564589a2020c7a3814
mourice-oduor <otienomaurice364@gmail.com>
2022-04-24 18:50:22 +0300

    Update PUT & DELETE serviceRoutes

--- node-api/service/productsService.js
+++ node-api/service/productsService.js
@@ -1,5 +1,6 @@
 const Product           = require('../models/productModel'),
-      {formatMongoData} = require('../helper/dbHelper');
+      {formatMongoData, checkObjectId} = require('../helper/dbHelper'),
+      constants         = require('../constants');
 
 module.exports.createProduct = async(serviceData) => {
     try {
@@ -20,6 +21,55 @@ module.exports.getProducts = async({ skip = 0, limit = 10 }) => {
         return formatMongoData(products);
     } catch (error) {
         console.log('Something went wrong: Service: getProducts', error)
+        throw new Error(error)
+    }
+   
+}
+
+module.exports.getProductById = async({ id }) => {
+    try {
+        checkObjectId(id);
+        let product = await Product.findById(id);
+        if (!product){
+            throw new Error(constants.productMessage.PRODUCT_NOT_FOUND)
+        }
+        return formatMongoData(product);
+    } catch (error) {
+        console.log('Something went wrong: Service: getProductById', error)
+        throw new Error(error)
+    }
+   
+}
+
+module.exports.updateProduct = async({ id, updateInfo }) => {
+    try {
+        checkObjectId(id);
+        let product = await Product.findOneAndUpdate(
+            { _id: id },
+            updateInfo,
+            { new: true }
+        );
+        if (!product){
+            throw new Error(constants.productMessage.PRODUCT_NOT_FOUND)
+        }
+        return formatMongoData(product);
+    } catch (error) {
+        console.log('Something went wrong: Service: getProductById', error)
+        throw new Error(error)
+    }
+   
+}
+
+module.exports.deleteProduct = async({ id }) => {
+    try {
+        checkObjectId(id);
+        let product = await Product.findByIdAndDelete(id);
+        if (!product){
+            throw new Error(constants.productMessage.PRODUCT_NOT_FOUND)
+        }
+        return formatMongoData(product);
+    } catch (error) {
+        console.log('Something went wrong: Service: getProductById', error)
         throw new Error(error)
     }
    
