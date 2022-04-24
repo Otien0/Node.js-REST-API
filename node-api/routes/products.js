9246ec7a3a3b4b4c6c0c5972f7d63b8f08332f39
mourice-oduor <otienomaurice364@gmail.com>
2022-04-25 00:46:21 +0300

    update ROUTE

--- node-api/routes/products.js
+++ node-api/routes/products.js
@@ -1,12 +1,10 @@
 const express               = require("express"),
       router                = express.Router(),
-    //   Product               = require("../models/productModel"),
       products              = require('../controllers/productsController'),
       { validateProduct, validatePaginationSchema, validateProductPut, validateToken }   = require('../middleware/validations'),
       Schema               = require('../apiSchema/JoiSchemas');
 
-
-
+      
 router.post('/', validateToken, validateProduct(Schema.ProductSchema), products.createProduct);
 
 router.get('/:id', validateToken, products.getProductById);
