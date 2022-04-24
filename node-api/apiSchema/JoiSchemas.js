f13bed9f0bd6ea86e3b3be0f36aff939195081d0
mourice-oduor <otienomaurice364@gmail.com>
2022-04-24 22:52:37 +0300

    added login schema

--- node-api/apiSchema/JoiSchemas.js
+++ node-api/apiSchema/JoiSchemas.js
@@ -28,3 +28,8 @@ module.exports.signup = Joi.object().keys({
     email: Joi.string().required(),
     password: Joi.string().required()
 })
+
+module.exports.loginSchema = Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+})
