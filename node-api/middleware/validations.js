0c25f1f0c95894995efb1f63cb5d8ff7f22f3eee
mourice-oduor <otienomaurice364@gmail.com>
2022-04-24 22:53:33 +0300

    update login validations

--- node-api/middleware/validations.js
+++ node-api/middleware/validations.js
@@ -1,4 +1,4 @@
-const { ProductSchema, getProductsPage , updateProductSchema, signup} = require('../apiSchema/JoiSchemas'),
+const { ProductSchema, getProductsPage , updateProductSchema, signup, loginSchema} = require('../apiSchema/JoiSchemas'),
       constants        = require('../constants/index');
       
       
@@ -87,6 +87,7 @@ module.exports.validateProductPut = () => {
 }
 
 // USER VALIDATIONS::
+// USER SIGNUP
 const validateUserSchema = (data) => {
     const result = signup.validate(data, {convert: false});
     if (result.error){
@@ -105,6 +106,35 @@ module.exports.validateUser = () => {
     return(req, res, next) => {
         let response = {...constants.defaultServerResponse}
         const error = validateUserSchema(req.body)
+        if(error){
+            response.body = error
+            response.message = constants.requestValidationMessage.BAD_REQUEST
+            return res.status(response.statusCode).send(response);
+        }
+        return next();
+    }
+    
+}
+
+//USER LOGIN
+const validateLogin = (data) => {
+    const result = loginSchema.validate(data, {convert: false});
+    if (result.error){
+        const errorDetails = result.error.details.map(value => {
+            return {
+                error: value.message,
+                path: value.path
+            }
+        })
+        return errorDetails;
+    }
+    return null;
+}
+
+module.exports.validateUserLogin = () => {
+    return(req, res, next) => {
+        let response = {...constants.defaultServerResponse}
+        const error = validateLogin(req.body)
         if(error){
             response.body = error
             response.message = constants.requestValidationMessage.BAD_REQUEST
