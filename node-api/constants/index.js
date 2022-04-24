f652f556c4bd7b44005e2c9398a3a3aed82ed995
mourice-oduor <otienomaurice364@gmail.com>
2022-04-24 18:47:52 +0300

    update constants

--- node-api/constants/index.js
+++ node-api/constants/index.js
@@ -6,10 +6,16 @@ module.exports = {
     },
     productMessage: {
         PRODUCT_CREATED: 'Product Created Successfully',
-        PRODUCT_FETCHED: 'Product Fetched Successfully'
+        PRODUCT_FETCHED: 'Product Fetched Successfully',
+        PRODUCT_UPDATED: 'Product Updated Successfully',
+        PRODUCT_DELETED: 'Product Deleted Successfully',
+        PRODUCT_NOT_FOUND: 'Product Not Found'
 
     },
     requestValidationMessage: {
         BAD_REQUEST: 'Invalid Fields'
+    },
+    databaseMessage: {
+        INVALID_ID: 'Invalid Id'
     }
 }
