07b77583fc6fde1e0a40567fa0a6ad16ae15edf9
mourice-oduor <otienomaurice364@gmail.com>
2022-09-08 23:48:12 +0300

    Update users route

--- node-api/routes/users.js
+++ node-api/routes/users.js
@@ -1,11 +1,10 @@
-const express               = require("express"),
-      router                = express.Router(),
-      users                 = require('../controllers/usersController'),
-      { validateUser, validateUserLogin } = require('../middleware/validations'),
-      Schema                = require('../apiSchema/JoiSchemas');
+const express = require("express"),
+  router = express.Router(),
+  users = require("../controllers/usersController"),
+  { validateUser, validateUserLogin } = require("../middleware/validations"),
+  Schema = require("../apiSchema/JoiSchemas");
 
-
-router.post('/signup', validateUser(Schema.signup), users.signup);
-router.post('/login', validateUserLogin(Schema.loginSchema), users.login);
+router.post("/signup", validateUser(Schema.signup), users.signup);
+router.post("/login", validateUserLogin(Schema.loginSchema), users.login);
 
 module.exports = router;
