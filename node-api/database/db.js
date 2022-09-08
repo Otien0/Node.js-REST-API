6576c865b40264147f2fed73ad5d9c78b65a7645
mourice-oduor <otienomaurice364@gmail.com>
2022-09-08 23:45:00 +0300

    Update database connection

---
+++ node-api/database/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotEnv   = require('dotenv');
+
+dotEnv.config();
+
+const dbUrl = process.env.DB_URL;
+
+const connectDB = () => {
+    mongoose.connect(dbUrl, { useUnifiedTopology: true , useNewUrlParser: true})
+    const connection = mongoose.connection
+
+    connection.on('connected' , () => {
+        console.log('MongoDB Database Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+}
+
+module.exports = connectDB
