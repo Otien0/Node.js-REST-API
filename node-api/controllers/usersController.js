3c7397d9d7dd9d2663af83df3d6779206861932d
mourice-oduor <otienomaurice364@gmail.com>
2022-09-08 23:44:44 +0300

    Update contoller

--- node-api/controllers/usersController.js
+++ node-api/controllers/usersController.js
@@ -1,35 +1,34 @@
-const userService    = require('../service/userService'),
-      constants      = require('../constants');
-
+const userService = require("../service/userService"),
+  constants = require("../constants");
 
 //USER-SIGNUP POST-ROUTE-- Add new user to DB
-module.exports.signup = async(req, res) => {
-    let response = {...constants.defaultServerResponse}
+module.exports.signup = async (req, res) => {
+  let response = { ...constants.defaultServerResponse };
 
-    try {
-        const serviceResponse = await userService.signup(req.body);
-        response.statusCode = 200;
-        response.message = constants.userMessage.SIGNUP_SUCCESS;
-        response.body = serviceResponse;
-    } catch (error) {
-        console.log('Something went wrong: Controller: signup', error);
-        response.message = error.message;
-    }
-    return res.status(response.statusCode).send(response);
-}
+  try {
+    const serviceResponse = await userService.signup(req.body);
+    response.statusCode = 200;
+    response.message = constants.userMessage.SIGNUP_SUCCESS;
+    response.body = serviceResponse;
+  } catch (error) {
+    console.log("Something went wrong: Controller: signup", error);
+    response.message = error.message;
+  }
+  return res.status(response.statusCode).send(response);
+};
 
 //USER-LOGIN POST-ROUTE-- Login user to DB
-module.exports.login = async(req, res) => {
-    let response = {...constants.defaultServerResponse}
+module.exports.login = async (req, res) => {
+  let response = { ...constants.defaultServerResponse };
 
-    try {
-        const serviceResponse = await userService.login(req.body);
-        response.statusCode = 200;
-        response.message = constants.userMessage.LOGIN_SUCCESS;
-        response.body = serviceResponse;
-    } catch (error) {
-        console.log('Something went wrong: Controller: login', error);
-        response.message = error.message;
-    }
-    return res.status(response.statusCode).send(response);
-}
+  try {
+    const serviceResponse = await userService.login(req.body);
+    response.statusCode = 200;
+    response.message = constants.userMessage.LOGIN_SUCCESS;
+    response.body = serviceResponse;
+  } catch (error) {
+    console.log("Something went wrong: Controller: login", error);
+    response.message = error.message;
+  }
+  return res.status(response.statusCode).send(response);
+};
