24675d056aa2ee1a2656537c20703a92410118a6
mourice-oduor <otienomaurice364@gmail.com>
2022-09-09 00:19:05 +0300

    Update user model

--- node-api/models/userModel.js
+++ node-api/models/userModel.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose'),
-      Schema   = mongoose.Schema;
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  validator = require("validator");
 
-      
-const userSchema = new Schema({
-    email : {
-        type : String,
-        required: true,
-        unique: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please Enter Your Email"],
+      unique: true,
+      validate: [validator.isEmail, "Please Enter a valid Email"],
+      unique: true,
     },
-    password : {
-        type: String,
-        required: true,
-        unique: true
-    }
-},
-    {
+    password: {
+      type: String,
+      required: [true, "Please Enter Your Password"],
+      minLength: [8, "Password should be greater than 8 characters"],
+      select: false,
+      unique: true,
+    },
+  },
+  {
     timestamps: true,
     toObject: {
-        transform: function(doc, ret, options) {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.password;
-            delete ret.__v;
-            return ret;
+      transform: function (doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
-        }
-    }
-})
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
